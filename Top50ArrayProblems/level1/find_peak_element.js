550708a9c1f6488b81c4c28c5027d4b56b59c3b7
GauravPatil301 <102862547+GauravPatil301@users.noreply.github.com>
2023-05-07 18:46:22 +0530

    peak element problem started

---
+++ Top50ArrayProblems/level1/find_peak_element.js
@@ -0,0 +1,14 @@
+// A javascript Program to find a peak element
+
+// Find the peak element in the array
+
+// Driver Code 
+let arr = [1, 3, 20, 4, 1, 0];
+let n = arr.length;
+console.log()
+
+function findPeak(arr,n) {
+    // first or last element is peak element
+    if(n === 1) return 0;
+
+}
