356c75df3917b32e51d1b12a9141749fe3cb74d8
GauravPatil301 <102862547+GauravPatil301@users.noreply.github.com>
2023-05-11 15:12:45 +0530

    reverse_array problem done

--- Top50ArrayProblems/level1/reverse_array.js
+++ Top50ArrayProblems/level1/reverse_array.js
@@ -10,3 +10,31 @@ function reverseArray(arr,start,end) {
     }
 }
 
+/* Utility function to print an array */
+function printArray(arr,size) {
+    for (let i = 0; i < array.length; i++) {
+        console.log(arr[i]);
+    }
+}
+
+/* Driver function to test above functions */
+let arr = [1,2,3,4,5,6]
+let n = 6;  
+// To Print original array
+printArray(arr,n);
+
+// Function Calling
+reverseArray(arr,0,n-1);
+
+console.log("Reversed array is:");
+
+// To print the Reversed array
+printArray(arr,n)
+
+/*output
+1 2 3 4 5 6
+Reversed array is
+6 5 4 3 2 1
+
+Time Complexity: O(n)
+*/
