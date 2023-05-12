94d067a0390413376122b6dfe254ecc426ada326
GauravPatil301 <102862547+GauravPatil301@users.noreply.github.com>
2023-05-12 10:29:33 +0530

    count_character_of_string

---
+++ Top50ArrayProblems/level1/count_character_of_string.js
@@ -0,0 +1,11 @@
+function countCharacter(array) {
+    // Create an empty object to hold character count
+    let charCount = {};
+
+    // Loop through each character in the string
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i];
+        
+    }
+
+}
--- Top50ArrayProblems/level1/find_peak_element.js
+++ Top50ArrayProblems/level1/find_peak_element.js
@@ -5,26 +5,18 @@
 // Driver Code 
 let arr = [1, 3, 20, 4, 1, 0];
 let n = arr.length;
-console.log("Index of a peak point is"+findPeak(arr,n));
+console.log(`Index of a peak element is ${findPeak(arr,n)}`)
 
 function findPeak(arr,n) {
     // first or last element is peak element
     if(n === 1) return 0;
-    if(arr[0] >= arr[1])return 0;
-    if(arr[n-1] >= arr[n-2]) return n - 1;
+    if(arr[0] >= arr[1]) return 0;
+    if(arr[n-1] >= arr[n-2]) return n-1;
 
     // check for every other element
     for (let i = 0; i < array.length; i++) {
+        const element = array[i];
         
-        // check if the neighbors are smaller
-        if(arr[i] >= arr[i-1] && arr[i] >= arr[i+1]) return i;      
     }
+
 }
-
-/*
-    Input: array[] = { 5, 10, 20, 15}
-    output: 20
-
-    Explanation: The element 20 has neighbors 10 and 15, both of them are less than 20.
-
-*/
